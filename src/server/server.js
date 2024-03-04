e7e671660ae0e34a5ae5ef7d64f881e61ab721f6
Tayokanch <tayoquadri11@gmail.com>
2024-03-04 21:15:02 +0000

    installed prisma and I added the Db url to the env file

---
+++ src/server/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+
+const app = express()
+app.disable('x-powered-by')
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+
