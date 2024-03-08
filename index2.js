d6ac394f63eba216495e84476c3a9d9f5a2d5b11
Tayokanch <tayoquadri11@gmail.com>
2024-03-08 07:06:07 +0000

    change location field to name

--- index2.js
+++ index2.js
@@ -1,5 +1,5 @@
 const NikeArt = {
-    "location": "Nike Art",
+    "name": "Nike Art",
     "category": "Art",
     "about": "Founded in 2009 by Nigerian textile artist, Nike Okundaye, Nike Art Gallery aims to positively transform the hitherto neglected arts and culture industry in Nigeria to ensure an enabling environment for the growth of African cultural heritage. The five-story building located in Lekki Peninsula holds over 8000 works of art, largely Nigerian and African, as well as Nike’s second textile museum. The gallery is a well-known cultural spot and tourist destination in West Africa.",
     "highlights": [
@@ -31,7 +31,7 @@ const NikeArt = {
 }
 
 const OshodiArtGallery = {
-    "location": "Oshodi Art Gallery",
+    "name": "Oshodi Art Gallery",
     "category": "Art",
     "about": "Oshodi Art Gallery, founded in 2015, stands as a hub of creative expression in Lagos, Nigeria. Emerging from the bustling Oshodi district, this gallery embodies the spirit of innovation and cultural vibrancy. Its inception marked a pivotal moment in Nigeria's art scene, fostering a community where artists and enthusiasts converge to celebrate creativity.",
     "highlights": [
@@ -64,7 +64,7 @@ const OshodiArtGallery = {
 }
 
 const YemisiShyllonArtMuseum = {
-    "location": "Yemisi Shyllon Art Museum",
+    "name": "Yemisi Shyllon Art Museum",
     "category": "Art",
     "about": "Established in 2017 by Yemisi Shyllon, a prominent Nigerian art collector and philanthropist, the Yemisi Shyllon Art Museum is a testament to his passion for preserving and promoting African art. Located within the Pan-Atlantic University campus in Lagos, Nigeria, the museum serves as a dynamic hub for cultural exchange and education, enriching the lives of students, scholars, and the general public alike.",
     "highlights": [
@@ -98,9 +98,10 @@ const YemisiShyllonArtMuseum = {
 
 
 "=====================BEACH======================"
+
 const HovBeachLagos = {
     "name": "Hov Beach Lagos",
-    "location": "Lagos, Nigeria",
+    "category": "Resort",
     "about": "Hov Beach Lagos offers a tranquil oasis in the bustling city, providing visitors with a serene escape along the coast of Lagos. Nestled within a picturesque setting, this beach destination combines natural beauty with modern amenities, catering to both relaxation seekers and adventure enthusiasts.",
     "highlights": [
         {
@@ -125,3 +126,91 @@ const HovBeachLagos = {
         }
     ]
 }
+
+const LandmarkBeach = {
+    "name": "Landmark Beach",
+    "category": "Resort",
+    "description": "Landmark Beach, situated in the upscale Victoria Island neighborhood of Lagos, offers a luxurious and tranquil escape from the city's hustle and bustle. With its pristine shoreline, upscale amenities, and picturesque surroundings, Landmark Beach is a premier destination for relaxation, recreation, and socializing.",
+    "highlights": [
+        {
+            "topic": "Scenic Beauty",
+            "description": "Experience the natural splendor of Landmark Beach with its pristine white sands, crystal-clear waters, and stunning views of the Atlantic Ocean. Whether you're lounging under the shade of a palm tree or taking a leisurely stroll along the shore, the beach's scenic beauty is sure to captivate."
+        },
+        {
+            "topic": "Luxurious Amenities",
+            "description": "Indulge in the luxurious amenities offered at Landmark Beach, including beachside cabanas, loungers, and umbrellas, as well as attentive beach attendants catering to your every need. With facilities such as showers, changing rooms, and restrooms available, your comfort and convenience are prioritized."
+        },
+        {
+            "topic": "Recreational Activities",
+            "description": "Engage in a variety of recreational activities at Landmark Beach, from swimming and sunbathing to beach volleyball and water sports. Whether you're seeking adrenaline-pumping adventures or laid-back leisure, there's no shortage of entertainment options to enjoy."
+        },
+        {
+            "topic": "Dining and Refreshments",
+            "description": "Savor the flavors of coastal cuisine and refreshing beverages at Landmark Beach's beachfront restaurants and bars. From fresh seafood delicacies to tropical cocktails and smoothies, there's something to tantalize every palate amidst the idyllic setting of the beach."
+        },
+        {
+            "topic": "Entertainment and Events",
+            "description": "Experience the vibrant entertainment scene at Landmark Beach, with live music performances, beach parties, and cultural events held regularly. Whether you're dancing under the stars or attending a beachside concert, the beach offers memorable experiences for all."
+        }
+    ],
+    "price": 45
+}
+
+const TakwaBayBeach = {
+    "name": "Takwa Bay Beach",
+    "category": "resort",
+    "description": "Takwa Bay Beach, located on the coastline of Lagos, Nigeria, is a popular destination known for its pristine sandy shores, clear turquoise waters, and serene atmosphere. Surrounded by picturesque cliffs and lush greenery, Takwa Bay offers visitors an idyllic retreat from the bustling city life of Lagos. Whether you're seeking relaxation, water sports, or simply enjoying the breathtaking views, Takwa Bay Beach has something to offer for everyone.",
+    "highlights": [
+        {
+            "topic": "Natural Beauty",
+            "description": "Immerse yourself in the natural beauty of Takwa Bay Beach, with its soft sandy shores, inviting warm waters, and stunning vistas of the Atlantic Ocean. Whether you're sunbathing, swimming, or simply taking a leisurely stroll along the coastline, the beach's tranquil ambiance and scenic surroundings will captivate your senses."
+        },
+        {
+            "topic": "Water Sports",
+            "description": "Embark on thrilling water sports adventures at Takwa Bay Beach, including surfing, jet skiing, kayaking, and banana boat rides. With its favorable wind and wave conditions, the beach is a haven for adrenaline junkies and water sports enthusiasts seeking excitement and adventure."
+        },
+        {
+            "topic": "Relaxation and Recreation",
+            "description": "Unwind and rejuvenate amidst the peaceful atmosphere of Takwa Bay Beach, where you can lounge under the shade of palm trees, enjoy a refreshing swim in the calm waters, or indulge in a beachside massage. Whether you're seeking solitude or socializing with fellow beachgoers, the beach offers ample opportunities for relaxation and recreation."
+        },
+        {
+            "topic": "Local Cuisine",
+            "description": "Sample delicious local cuisine and refreshing beverages at the beachside eateries and bars scattered along Takwa Bay Beach. From grilled seafood delicacies to tropical fruit juices and cocktails, you can savor the flavors of Nigeria while enjoying the laid-back vibe of the beach."
+        },
+        {
+            "topic": "Cultural Experience",
+            "description": "Immerse yourself in the vibrant culture and traditions of Nigeria at Takwa Bay Beach, where you can interact with friendly locals, participate in traditional drumming and dancing, and shop for authentic handicrafts and souvenirs. Whether you're attending a beachside festival or learning about local customs, the beach offers a unique cultural experience."
+        }
+    ],
+    "price": 20 
+}
+
+
+const LaCampagneResort = {
+    "name": "La Campagne Tropicana Beach Resort",
+    "category": "Resort",
+    "description": "La Campagne Tropicana Beach Resort is a luxurious beachfront resort nestled along the picturesque coastline of Lekki, Lagos State. Set amidst lush tropical gardens and overlooking the sparkling waters of the Atlantic Ocean, this idyllic retreat offers guests a perfect blend of natural beauty, serene ambiance, and world-class hospitality. Whether you're seeking relaxation, adventure, or cultural immersion, La Campagne Tropicana Beach Resort promises an unforgettable experience.",
+    "highlights": [
+        {
+            "topic": "Luxurious Accommodation",
+            "description": "Indulge in luxurious accommodation options at La Campagne Tropicana Beach Resort, ranging from elegant beachfront villas and chalets to cozy cottages and suites. Each accommodation is tastefully designed with traditional African decor and modern amenities, offering guests a comfortable and unforgettable stay."
+        },
+        {
+            "topic": "Beachfront Paradise",
+            "description": "Experience the beauty of pristine white sands and azure waters at La Campagne Tropicana's private beach. Whether you're lounging under the shade of palm trees, swimming in the clear waters, or enjoying thrilling water sports activities, the resort's beachfront paradise provides endless opportunities for relaxation and recreation."
+        },
+        {
+            "topic": "Cultural Immersion",
+            "description": "Immerse yourself in the rich culture and traditions of Nigeria with a variety of cultural activities and experiences offered at La Campagne Tropicana Beach Resort. From traditional dance performances and drumming sessions to art and craft workshops, guests can gain insight into the vibrant heritage of the region."
+        },
+        {
+            "topic": "Gourmet Dining",
+            "description": "Savor exquisite culinary delights at the resort's on-site restaurants, where talented chefs create a fusion of local and international cuisines using fresh, locally sourced ingredients. Whether you're dining al fresco by the beach or enjoying a romantic dinner under the stars, La Campagne Tropicana offers an unforgettable gastronomic journey."
+        },
+        {
+            "topic": "Relaxation and Wellness",
+            "description": "Rejuvenate your mind, body, and soul at the resort's wellness center, offering a range of holistic treatments and therapies inspired by African healing traditions. From massages and facials to yoga and meditation sessions, guests can unwind and rejuvenate amidst the tranquil surroundings of La Campagne Tropicana Beach Resort."
+        }
+    ],
+    "price": 50
+};
